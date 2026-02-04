f6bd7224a5e2034c6dd7dd6406dd37d3f6ae7f2a
taka-dad <takako.12047@gmail.com>
2026-02-04 21:55:41 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class Program
+{
+        static void Main(string[] args)
+        {
+        //int a = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+        string Number = input;
+        int Count = 0;
+        int Firstnum = Number.IndexOf("1", 0);
+        int Secondnum = Number.IndexOf("1", 1);
+        int Thirdnum = Number.IndexOf("1", 2);
+
+
+        if (Firstnum == 0)
+        {
+            Count++;
+        }
+        else
+        { }
+        if (Secondnum == 1)
+        {
+            Count++;
+            
+        }
+        else
+        { }
+        if (Thirdnum == 2)
+        {
+            Count++;
+        }
+        else 
+        { }
+
+            Console.WriteLine(Count);
+        Console.ReadLine();
+
+    }
+}
