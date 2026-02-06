8a386589b7b479225c39d26413598922dcc20c5d
taka-dad <takako.12047@gmail.com>
2026-02-06 22:05:36 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+        static void Main(string[] args)
+        {
+        //数値の読み込み
+        int? SumNumber = int.Parse(Console.ReadLine());
+        string[]? AllNumber = Console.ReadLine().Split(' ');
+
+        
+        int Count = 0;
+        for (int i = 0; (int.Parse(AllNumber[i])) % 2 == 0&(i < SumNumber); i++)
+        {
+            AllNumber[i] = ((int.Parse(AllNumber[i])) / 2).ToString();
+            if (i == SumNumber - 1)
+            {
+                Count += 1;
+                i = 0;
+            }
+        }
+        
+
+        Console.WriteLine(Count);
+        Console.ReadLine();
+
+    }
+}
